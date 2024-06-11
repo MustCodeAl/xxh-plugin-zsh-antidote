97dbfda8b477f6a4ed233699098df806afd54dee
Albert <87888006+MustCodeAl@users.noreply.github.com>
2024-06-11 01:07:13 -0500

    Update pluginrc.zsh
    add plugin name

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -1,4 +1,12 @@
 # This script will be executed ON THE HOST when you connect to the host.
 # Put here your functions, environment variables, aliases and whatever you need.
-
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+plugin_name='xxh-plugin-zsh-antidote'
+
+
+export ZSH="$CURR_DIR/antidote"
+
+
+
+
+
